62e0dc57caeb0fc22cab1bde38f2e4ae5bd00b56
ziyua <z.y.ma@qq.com>
2014-11-22 01:01:56 +0100

    set %ip% for what?

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
-import random
+# import random
 import SocketServer
 import sys
 import os
@@ -15,13 +15,14 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
-
-    PROTOCOL = r'(INVITE|ACK|BYE|CANCEL|OPTIONS|REGISTER)\ssip:\w+@(\w+|\d+(\.\d+){3}):\d+\sSIP/2.0'
-    PORT_RTP = random.randint(1024, 65535)
+    ALLOW = 'INVITE|ACK|BYE|CANCEL|OPTIONS|REGISTER'
+    PROTOCOL = r'(' + ALLOW + ')\ssip:\w+@(\w+|\d+(\.\d+){3}):\d+\sSIP/2.0'
+    PORT_RTP = 23032  # random.randint(1024, 65535)
 
     def send_single(self, text):
         print 'Send -- ', text[:-4]
-        self.socket.sendto(text, self.client_address)  # send Tring Ring and ACK # Port sip: 5060
+        # send Tring Ring and ACK # Port sip: 5060
+        self.socket.sendto(text, self.client_address)
         # self.wfile.write(text)
 
     def handle(self):
@@ -32,29 +33,30 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
             m = re.match(self.PROTOCOL, line)
             if not m is None:
                 if line.split()[0] == 'INVITE':
-                    # INVITE: client --> servidor: Src Port (client): ca-2 (5065),          Dst Port (servidor): sip (5060) igual a ACK  or BYE
+                    # INVITE: client 5065 --> servidor 5060
                     # 100 Trying
                     self.send_single('SIP/2.0 100 Trying\r\n\r\n')
                     # 180 Ring
                     self.send_single('SIP/2.0 180 Ring\r\n\r\n')
-                    # Send 200 OK
-                    self.send_single('SIP/2.0 200 OK\r\nm=audio ' + str(self.PORT_RTP) + ' RTP/AVP 8 101\r\n\r\n')
+                    # Send 200 OK, No hay port random. no need 'sdp'
+                    # sdp = 'm=audio ' + str(self.PORT_RTP) + \
+                    #       ' RTP/AVP 8 101\r\n'
+                    # self.send_single('SIP/2.0 200 OK\r\n' + sdp + '\r\n')
+                    self.send_single('SIP/2.0 200 OK\r\n\r\n')
                 elif line.split()[0] == 'ACK':
-                    # ACK client -> servidor: Src Port (client)   : ca-2 (5065),           Dst Port (servidor)  : sip (5060); igual a INVITE  or BYE
-                    #                                                                                                  |receive Port (Peer a Peer?)Puedo difinir 5060 ACK INVITE and BYE.)
-                    #                                                                ++++++++++++son servidor+++++++++++
-                    #                                                                |send Port (No puedo definir en mp32rtp.)
-                    # RTP In sip.cap:         Src Port (servidor) : telelpathstart (5010), Dst Port (client)    : 12440 (12440)
-                    #                 Dst port es <Random />.
-                    #                 Src port es <5010 />
+                    # ACK client 5065 -> servidor 5060
+                    # RTP servidor 5010 --> client 12440 (random)
                     ip = self.client_address[0]
-                    aEjecutar = './mp32rtp -i ' + ip + ' -p ' + str(self.PORT_RTP) + ' < ' + sys.argv[3]  # Port '23032' Dst es <Rondom />
+                    aEjecutar = './mp32rtp -i ' + ip + ' -p ' \
+                                + '23032 < ' + sys.argv[3]
+                    #            + str(self.PORT_RTP) + ' < ' + sys.argv[3]
                     print "Vamos a ejecutar $ ", aEjecutar
-                    os.system(aEjecutar)  # 1. otro task
+                    os.system(aEjecutar)  # otro task mejor
+                    # client espera 5 seg.
                     print '-- Send end --'
                 elif line.split()[0] == 'BYE':
                     # Solo Send 200 OK
-                    # BYE (client -> servidor): Src Port (client): ca-2 (5065), Dst Port (servidor): sip (5060); igual a INVITE  or ACK
+                    # BYE: client 5065 -> servidor 5060
                     self.send_single('SIP/2.0 200 OK\r\n\r\n')
                 else:
                     # YES -> INVITE|ACK|BYE   NOT -> CANCEL|OPTIONS|REGISTER
@@ -68,6 +70,7 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
 if __name__ == "__main__":
     if len(sys.argv) != 4 or not os.path.exists(sys.argv[3]):
+        # 1. pregunta -> IP servie para que?
         sys.exit('Usage: python server.py IP port audio_file')
     else:
         # Creamos servidor de eco y escuchamos
