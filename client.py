0b667af713772fde842c59b4729bd1871d2a227c
ziyua <z.y.ma@qq.com>
2014-11-22 01:02:42 +0100

    QUE ES RuntimeError: maximum recursion depth exceeded ?

--- client.py
+++ client.py
@@ -4,9 +4,22 @@
 Programa cliente que abre un socket a un servidor
 """
 
+import SocketServer
 import socket
 import sys
 import re
+
+
+class RTPhandle(SocketServer.DatagramRequestHandler):
+
+    def handle(self):
+        while 1:
+            data = self.rfile.read()
+            print 'play data audio:', repr(data)
+            if data and len(data) != 0:
+                RTPserv.handle_request()
+            else:
+                break
 
 
 class SIPclient():
@@ -14,46 +27,36 @@ class SIPclient():
     def __init__(self, Addr, User):
         self.PROTOCOL = r'^SIP/2.0\s\d{3}\s\w+'
         self.User = User
+        self.recvRTP = False
+        self.Method = ''
         # Cliente UDP simple.
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        # Bind Address: jose@12.34.56.78(ip):5060(port) -> Addr = (12.34.56.78, 5060)
+        # jose@12.34.56.78(ip):5060(port) -> Addr = (12.34.56.78, 5060)
         self.my_socket.connect(Addr)
 
     def sendSIP(self, Method):
+        self.Method = Method
         LINE = Method + ' sip:' + self.User + ' SIP/2.0\r\n'
         print "Enviando: " + LINE
         self.my_socket.send(LINE + '\r\n')
 
-    def receiveRTP(self, portRTP):
-        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.my_socket.bind(('', portRTP))
-
-        while 1:
-            data = self.my_socket.recv(1024)
-            print data
-
-    recvRTP = False
-
     def receiveSIP(self, bufferSiza=1024):
         try:
-            data, addrs = self.my_socket.recvfrom(bufferSiza)
+            data = self.my_socket.recv(bufferSiza)
         except socket.error:
-            sys.exit('Error: No server listening at ' + SERVER + ' port ' + str(PORT))
+            sys.exit('Error: No server listening at ' +
+                     SERVER + ' port ' + str(PORT))
 
         # SIP/2.0 100/180/200 Trying/Ring/Ok
         data = data.strip()
-        print 'Receive -- ', repr(data), len(data), addrs
+        print 'Receive -- ', repr(data)
         mat = re.match(self.PROTOCOL, data)
         if mat is not None:
             Num = data.split()[1]
             if Num == "200":
-                self.sendSIP('ACK')
-                if self.recvRTP:
-                    portRTP = int(data.split()[4])
-                    self.receiveRTP(portRTP)
-                    self.recvRTP = False
+                if self.Method != 'BYE':
+                    self.sendSIP('ACK')
             elif Num == "100":  # SIP/2.0 Trying 100
                 self.recvRTP = True
                 self.receiveSIP()
@@ -63,8 +66,12 @@ class SIPclient():
             print 'Not aquì RTP, if echo ERROR', repr(data)
         # else: No correcta msg que envia por servidor.
 
+    def startRTP(self):
+        sRTP = self.recvRTP
+        self.recvRTP = False
+        return sRTP
+
     def closeSIP(self):
-        print "Terminando socket..."
         self.my_socket.close()
         print "Fin."
 
@@ -81,10 +88,17 @@ if __name__ == '__main__':
         sys.exit(Usage)
 
     # Init SIPclient
-    mySIP = SIPclient((SERVER, PORT), sys.argv[2])
+    mySIP = SIPclient((SERVER, PORT), User=sys.argv[2])
 
     # vamos a enviar (INVITE, jose@127.0.0.1:8000)
-    mySIP.sendSIP(sys.argv[1])
-
+    mySIP.sendSIP(Method=sys.argv[1])
     mySIP.receiveSIP()
+    if mySIP.startRTP():
+        print 'Starting RTP...'
+        RTPserv = SocketServer.UDPServer(("", 23032), RTPhandle)
+        # exprid time 5.0 seg
+        # if 5 seg not receive RTP data(no hay audio), break
+        RTPserv.timeout = 5.0
+        RTPserv.handle_request()
+    # solo need close socket SIP, RTP ya termino.
     mySIP.closeSIP()
